a17d99125c16646f284ac0eee4df958a10fb998b
Documenter.jl <documenter@juliadocs.github.io>
2025-04-07 07:03:51 +0000

    build based on 9bf8f2a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FreeEnergyMachine","category":"page"},{"location":"#FreeEnergyMachine","page":"Home","title":"FreeEnergyMachine","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FreeEnergyMachine.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FreeEnergyMachine]","category":"page"}]
+}
